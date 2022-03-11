fabbf45421ff42431de2c521e62d2e1c88f64d7f
sabbir <sabbirhossen58sh58gmail.com>
2022-03-11 08:43:05 +0600

    'added_all-setServer'

--- index.js
+++ index.js
@@ -17,8 +17,17 @@ async function run() {
     console.log('hit the database');
     try{
         await client.connect();
+
+        //shop data added
         const database =client.db('CycleShop');
         const booksCollection = database.collection('allCycle')
+        //order data added
+        const databases =client.db('CycleShop');
+        const orderCollection = databases.collection('allOrder')
+        //reviews data added
+        const databasees =client.db('CycleShop');
+        const  reviewCollection = databasees.collection('allReview')
+        //-------------------- add json data cycle shop -----------------------
         //Get api 
         app.get('/allCycle', async(req ,res)=>{
             const cursor= booksCollection.find({});
@@ -34,12 +43,12 @@ async function run() {
         })
  
  
-     //    POST API
+     //    POST API shop data add
         app.post('/allCycle', async(req,res)=>{
             const cycle = req.body
-            console.log("hit the post api " ,cycle);
+            // console.log("hit the post api " ,cycle);
          const result = await   booksCollection.insertOne(cycle)
-         console.log(result);
+        //  console.log(result);
         //  res.send('post hitted')
          res.json(result)
         });
@@ -49,7 +58,50 @@ async function run() {
             const cycle=await booksCollection.deleteOne(query)
             res.json(cycle) 
         })
- 
+        //--------------------End add json data cycle shop -----------------------
+        // ------------------------- Add to All order data -------------------------
+        // post api order
+        app.post('/allOrder', async(req,res)=>{
+            let order = req.body
+            order.createdAt = new Date ();
+            // console.log('hit the order database', order);
+
+            const results = await orderCollection.insertOne(order)
+            res.json(results)           
+            // res.send('data hitted')
+        } )
+        app.get('/allOrder', async(req,res)=>{
+            const cursor= orderCollection.find({});
+            const order = await cursor.toArray()
+            res.send(order);
+        })
+        app.delete('/allOrder/:Id', async (req,res)=>{
+            const id =req.params.Id;
+            const query = {_id: ObjectId(id)}
+            const order=await orderCollection.deleteOne(query)
+            res.json(order) 
+        })
+         // -------------------------End  Add to All order data -------------------------
+         app.post('/allReview', async(req,res)=>{
+            let review = req.body
+            review.createdAt = new Date ();
+            console.log('hit the order database', review);
+
+            const results = await reviewCollection.insertOne(review)
+            res.json(results)           
+            res.send('data hitted')
+        } )
+        app.get('/allReview', async(req,res)=>{
+            const cursor= reviewCollection.find({});
+            const review = await cursor.toArray()
+            res.send(review);
+        })
+        app.delete('/allReview/:Id', async (req,res)=>{
+            const id =req.params.Id;
+            const query = {_id: ObjectId(id)}
+            const review=await reviewCollection.deleteOne(query)
+            res.json(review) 
+        })
     }   
     finally{
         // await client.close();
